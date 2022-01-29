33aec6af10caf9eadabfb371622cede34bd304bb
Kamrul Hasan <you@example.com>
2022-01-29 15:14:42 +0600

    String concatenation done using function

---
+++ string-concatenation.js
@@ -0,0 +1,9 @@
+// String concatenation using function
+function fullName(firstName, lastName) {
+    var result = firstName + ' ' + lastName;
+    return result;
+}
+var fName = "Kamrul";
+var lName = "Hasan";
+var nameResult = fullName(fName, lName);
+console.log(nameResult);
