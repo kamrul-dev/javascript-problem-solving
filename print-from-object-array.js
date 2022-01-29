109952b5d1827c5804c6739fb0da2b4227d868dc
Kamrul Hasan <you@example.com>
2022-01-29 15:32:33 +0600

    print string form an object array

---
+++ print-from-object-array.js
@@ -0,0 +1,11 @@
+// Print value form object array
+
+const pizza = {
+    toppings: ['cheese', 'sauce', 'pepperoni'],
+    crust: 'deep dish',
+    serves: 2
+}
+
+var result = pizza.toppings[2];
+console.log(result);
+// console.log(pizza.toppings[2]);
