48d3df153642f7a5d5aaf1d280f334b001e9fffc
Kamrul Hasan <you@example.com>
2022-01-29 15:23:40 +0600

    square result done

---
+++ square.js
@@ -0,0 +1,9 @@
+// do square any number
+
+function squareNumber(squareNum){
+    var result = Math.pow(squareNum, 2);
+    return result;
+}
+var sqNumber = 5;
+var squareResult = squareNumber(sqNumber);
+console.log(squareResult);
