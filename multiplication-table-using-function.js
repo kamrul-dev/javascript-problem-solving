2489bfad6dbe9ad91c423c44fbb4f41f8cc48bed
Kamrul Hasan <you@example.com>
2022-01-29 13:54:37 +0600

    multiplication table

---
+++ multiplication-table-using-function.js
@@ -0,0 +1,10 @@
+// Multiplication table using function
+
+function multiplicationTable(){
+    for( var i = 1; i <= 10; i++){
+        var mul = 13;
+        mul *= i;
+        console.log(mul);
+    }
+}
+multiplicationTable();
