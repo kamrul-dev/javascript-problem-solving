6a6b72fdeb884a2899568d8253bc83a89766ab24
Kamrul Hasan <you@example.com>
2022-01-29 14:05:37 +0600

    regular to lower case and regular to uppercase letter

---
+++ regular-to-lowercase-letter.js
@@ -0,0 +1,18 @@
+// Any regular case to lowercase letter using function
+
+function lowercaseLetter(){
+    var name = "KamRuL";
+    var resultForLowerCase = name.toLowerCase();
+    console.log(resultForLowerCase);
+}
+lowercaseLetter();
+
+
+
+// regular case to uppercase
+function uppercaseLetter(){
+    var name = "KamRuL";
+    var resultForLowerCase = name.toUpperCase();
+    console.log(resultForLowerCase);
+}
+uppercaseLetter();
