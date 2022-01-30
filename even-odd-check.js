4ece09ea09bc5f0a74e5b87470afb39c091c3cd6
Kamrul Hasan <you@example.com>
2022-01-30 15:17:19 +0600

    check even and odd number

--- even-odd-check.js
+++ even-odd-check.js
@@ -1,1 +1,14 @@
 // check given number is even or odd.  if number is even then returns true. if it is odd then returns odd.
+
+function isEvenOdd(number){
+    if(number % 2 == 0){      // conditon for odd  if(number % 2 != 0)
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+let getEven = 20;
+const isEven = isEvenOdd(getEven);
+console.log('Is the number even: ', isEven);
--- leap-year.js
+++ leap-year.js
@@ -1,4 +1,4 @@
-// check given year is leap year. If it is leap year then return ture. otherwise return false.
+// check given year is leap year. If it is leap year then return true. otherwise return false.
 
 function leapYear(year){
     if((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)){
