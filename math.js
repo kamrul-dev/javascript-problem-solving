5b458208cf59730d8f8fb18a63601513526a85fe
Kamrul Hasan <you@example.com>
2022-02-01 00:04:52 +0600

    Swap variable, swap without temp, destructing

--- math.js
+++ math.js
@@ -1,8 +1,9 @@
 
+// Some Mathematical functions
 // const myNumber = -5;
 // const output = Math.abs(myNumber);
 
-const myNumber = 21.95;
+// const myNumber = 21.95;
 // const output = Math.ceil(myNumber);
 // const output = Math.floor(myNumber);
 // const output = Math.round(myNumber);
@@ -10,9 +11,9 @@ const myNumber = 21.95;
 // const rounded = Math.round(output);
 // console.log(rounded);
 
-
-for(let i = 0; i <= 30; i++ ){
-    const output = Math.random() * 6;
-    const rounded = Math.round(output);
-    console.log(rounded);
-}
+// for(let i = 0; i <= 30; i++ ){
+//     const output = Math.random() * 6;
+//     const rounded = Math.round(output);
+//     console.log(rounded);
+// }
+//=================================================
